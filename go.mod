2ac240518393ad5064d6eb37363e26335892dc26
Hengfang Wang <wanghengfang@hyperchain.cn>
2021-06-20 23:00:25 +0800

    feat(init): init project

---
+++ go.mod
@@ -0,0 +1,11 @@
+module cmapp
+
+go 1.13
+
+require (
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/spf13/cobra v1.1.3
+	github.com/spf13/viper v1.8.0
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
---
+++ pkg/ma/api.go
@@ -0,0 +1,36 @@
+/*
+ * Copyright © 2021 zibuyu28
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ma
+
+// create 这个命令，包含了如何调用api的流程，
+// 也就是说create这个命令是如何调用 mengine
+// 这个接口的里面的方法的，是由create这个命令
+// 决定的
+
+type MEngine interface {
+	// create 相关
+
+	// delete 相关
+
+
+	// startUp 相关
+
+
+	// shutdown 相关
+
+
+}
